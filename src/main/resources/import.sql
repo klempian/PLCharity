8645afb5ca795d4bea14b08ee4bc6dffcc5c5b48
Ian Klemp <klemp.ian@gmail.com>
2020-05-05 18:35:32 +0200

    sample data categories

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,3 +2,5 @@ INSERT INTO institutions (name, description) VALUES ('Fundacja "Dbam o Zdrowie"'
 INSERT INTO institutions (name, description) VALUES ('Fundacja "A kogo"', 'Cel i misja: Pomoc wybudzaniu dzieci ze śpiączki.');
 INSERT INTO institutions (name, description) VALUES ('Fundacja “Dla dzieci"', 'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.');
 INSERT INTO institutions (name, description) VALUES ('Fundacja “Bez domu”', 'Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.');
+
+INSERT INTO categories (name) VALUES ('ubrania, które nadają się do ponownego użycia'), ('ubrania, do wyrzucenia'), ('zabawki'), ('książki'), ('inne');
