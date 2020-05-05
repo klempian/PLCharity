b0de60fdb5ba47d3b8737f3dfdab46001c85e112
Ian Klemp <klemp.ian@gmail.com>
2020-05-05 13:31:17 +0200

    Category entity and repository

---
+++ src/main/java/pl/coderslab/charity/model/Category.java
@@ -0,0 +1,21 @@
+package pl.coderslab.charity.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
