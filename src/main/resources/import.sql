f9f08f459675598301360626539608eb46a1fd09
Ian Klemp <klemp.ian@gmail.com>
2020-05-05 17:55:13 +0200

    sample data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO institutions (name, description) VALUES ('Fundacja "Dbam o Zdrowie"', 'Cel i misja: Pomoc dzieciom z ubogich rodzin.');
+INSERT INTO institutions (name, description) VALUES ('Fundacja "A kogo"', 'Cel i misja: Pomoc wybudzaniu dzieci ze śpiączki.');
+INSERT INTO institutions (name, description) VALUES ('Fundacja “Dla dzieci"', 'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.');
+INSERT INTO institutions (name, description) VALUES ('Fundacja “Bez domu”', 'Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.');
