df5850323671fc4acf11a5c82057d55c30d2a214
Ian Klemp <klemp.ian@gmail.com>
2020-05-05 18:57:40 +0200

    donation count and quantity model attribute for front page

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.charity.model.Institution;
+import pl.coderslab.charity.repository.DonationRepository;
 import pl.coderslab.charity.repository.InstitutionRepository;
 
 import java.util.List;
@@ -14,10 +15,12 @@ import java.util.List;
 public class HomeController {
 
     private InstitutionRepository institutionRepository;
+    private DonationRepository donationRepository;
 
     @Autowired
-    public HomeController(InstitutionRepository institutionRepository) {
+    public HomeController(InstitutionRepository institutionRepository, DonationRepository donationRepository) {
         this.institutionRepository = institutionRepository;
+        this.donationRepository = donationRepository;
     }
 
 
@@ -26,6 +29,12 @@ public class HomeController {
 
         List<Institution> institutions = institutionRepository.findAll();
         model.addAttribute("institutions", institutions);
+
+        Long quantitySum = donationRepository.countDonationsQuantitySum();
+        model.addAttribute("quantitySum", quantitySum == null ? 0 : quantitySum);
+
+        Long donationCount = donationRepository.count();
+        model.addAttribute("donationCount", donationCount);
 
         return "index";
     }
