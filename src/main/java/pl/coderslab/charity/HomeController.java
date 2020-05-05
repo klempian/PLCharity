ef493c61803ae891b9bba5e2cb6540f6d5b59a58
Ian Klemp <klemp.ian@gmail.com>
2020-05-05 17:44:46 +0200

    institution list model attribute for front page

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -1,16 +1,32 @@
 package pl.coderslab.charity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.model.Institution;
+import pl.coderslab.charity.repository.InstitutionRepository;
+
+import java.util.List;
 
 
 @Controller
 public class HomeController {
 
+    private InstitutionRepository institutionRepository;
+
+    @Autowired
+    public HomeController(InstitutionRepository institutionRepository) {
+        this.institutionRepository = institutionRepository;
+    }
+
 
     @RequestMapping("/")
     public String homeAction(Model model){
+
+        List<Institution> institutions = institutionRepository.findAll();
+        model.addAttribute("institutions", institutions);
+
         return "index";
     }
 }
