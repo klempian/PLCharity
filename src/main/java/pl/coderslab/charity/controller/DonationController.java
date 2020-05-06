f0334a96772db00efaa4eeff5902db89a8dfb573
Ian Klemp <klemp.ian@gmail.com>
2020-05-06 15:03:46 +0200

    save donation and thank you page

--- src/main/java/pl/coderslab/charity/controller/DonationController.java
+++ src/main/java/pl/coderslab/charity/controller/DonationController.java
@@ -3,9 +3,11 @@ package pl.coderslab.charity.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.charity.model.Donation;
 import pl.coderslab.charity.repository.CategoryRepository;
+import pl.coderslab.charity.repository.DonationRepository;
 import pl.coderslab.charity.repository.InstitutionRepository;
 
 @Controller
@@ -14,10 +16,12 @@ public class DonationController {
 
     private CategoryRepository categoryRepository;
     private InstitutionRepository institutionRepository;
+    private DonationRepository donationRepository;
 
-    public DonationController(CategoryRepository categoryRepository, InstitutionRepository institutionRepository) {
+    public DonationController(CategoryRepository categoryRepository, InstitutionRepository institutionRepository, DonationRepository donationRepository) {
         this.categoryRepository = categoryRepository;
         this.institutionRepository = institutionRepository;
+        this.donationRepository = donationRepository;
     }
 
     @GetMapping("/")
@@ -30,6 +34,13 @@ public class DonationController {
         model.addAttribute("institutions", institutionRepository.findAll());
 
         return "form";
+    }
+
+    @PostMapping("/")
+    public String saveDonation(Donation donation) {
+
+        donationRepository.save(donation);
+        return "thankyou";
     }
 
 }
