c6ff487074495d4262a1537f2d38e52661b23adb
Ian Klemp <klemp.ian@gmail.com>
2020-05-08 19:19:06 +0200

    success handler

--- src/main/java/pl/coderslab/charity/SecurityConfig.java
+++ src/main/java/pl/coderslab/charity/SecurityConfig.java
@@ -1,8 +1,8 @@
 package pl.coderslab.charity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,12 +15,14 @@ import pl.coderslab.charity.service.SpringDataUserDetailsService;
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    CustomAuthenticationSuccessHandler successHandler = new CustomAuthenticationSuccessHandler();
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin()
                 .loginPage("/login")
                 .loginProcessingUrl("/login")
-                .defaultSuccessUrl("/",true)
+                .successHandler(successHandler)
                 .failureUrl("/login?error=true")
         ;
 
